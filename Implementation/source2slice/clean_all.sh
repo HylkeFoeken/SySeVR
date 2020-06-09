38738ff2252e6c73bdfde90f409f05beab5ebc6b
Hylke Foeken <h.foeken@gmail.com>
2020-06-09 15:17:18 +0200

    scripts added to run stage 1 of SySeVR

---
+++ Implementation/source2slice/clean_all.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+#########################
+# clean all module 1 output
+#########################
+
+rm -rf cfg_db/*
+rm -rf pdg_db/*
+rm -rf dict_call2cfgNodeID_funcID/*
+rm -rf test_data/4/labels/* 
+rm -rf test_data/4/slices/* 
+rm -rf test_data/4/vulnlines/* 
+rm sensifunc_slice_points.pkl pointuse_slice_points.pkl arrayuse_slice_points.pkl integeroverflow_slice_points_new.pkl
---
+++ Implementation/source2slice/kill_neo.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+#########################
+# kills neo4j out of memory process
+#########################
+killall -9 /usr/lib/jvm/zulu-7-amd64/bin/java
