799365cbd5c86d75d726df5701c6979aa2b3b35c
Hylke Foeken <h.foeken@gmail.com>
2020-06-09 15:57:59 +0200

    strip the leading zero's of the dictionary keys fixed check of verified vulnerable line of code with line of code in SARD vulnerable line of code

--- Implementation/source2slice/make_label.py
+++ Implementation/source2slice/make_label.py
@@ -2,20 +2,24 @@
 import os
 import pickle
 
-
 f = open("dict_cwe2father.pkl", 'rb')
 dict_cwe2father = pickle.load(f)
 f.close()
 
-#print dict_cwe2father['CWE-787']
+# print dict_cwe2father['CWE-787']
 
 f = open("label_vec_type.pkl", 'rb')
 label_vec_type = pickle.load(f)
 f.close()
 
-f = open("dict_testcase2code.pkl",'rb')
+f = open("dict_testcase2code.pkl", 'rb')
 dict_testcase2code = pickle.load(f)
 f.close()
+
+# HF: keys are prefixed with zero's which causes them not te be found in make_label()
+#     this strips the leading zero's of the dictionary keys
+dict_testcase2code = {key.lstrip('0'): value
+                      for key, value in dict_testcase2code.iteritems()}
 
 
 def get_label_veclist(list_cwe):
@@ -55,8 +59,8 @@ def make_label(path, dict_vuln2testcase, _type):
     list_all_vulline = []
     for _slice in context:
         vulline = []
-        index_line = _slice.split('\n')[1] 
-        list_codes = _slice.split('\n')[2:-1] 
+        index_line = _slice.split('\n')[1]
+        list_codes = _slice.split('\n')[2:-1]
         case_name = index_line.split(' ')[1]
         key_name = '/'.join(index_line.split(' ')[1].split('/')[-2:])
         print index_line
@@ -67,18 +71,18 @@ def make_label(path, dict_vuln2testcase, _type):
 
             _dict_cwe2line_target = {}
             _dict_cwe2line = {}
-			for _dict in dict: 
+            for _dict in dict:
                 for key in _dict.keys():
                     if _dict[key] not in _dict_cwe2line_target.keys():
-                        _dict_cwe2line_target[_dict[key]] = [key] 
+                        _dict_cwe2line_target[_dict[key]] = [key]
                     else:
                         _dict_cwe2line_target[_dict[key]].append(key)
 
-                
                 for line in list_codeline:
                     line = line.strip()
                     if line in _dict.keys():
-                        if not ' '.join((list_codes[list_codeline.index(line)].strip()).split(' ')[:-1]) == dict_testcase2code[key_name+"/"+line].strip():
+                        if not ' '.join((list_codes[list_codeline.index(line)].strip()).split(' ')[:-1]).replace(' ', '') == \
+                                   dict_testcase2code[key_name + "/" + line].replace(' ', '').strip():
                             continue
                         cweid = _dict[line]
                         vulline.append(list_codeline.index(line))
@@ -112,7 +116,7 @@ def make_label(path, dict_vuln2testcase, _type):
             else:
                 list_vuln_cwe = list(set(list_vuln_cwe))
                 list_label = get_label_veclist(list_vuln_cwe)
-            
+
         else:
             list_label = [0] * len(label_vec_type)
 
@@ -128,40 +132,61 @@ def main():
     f.close()
     _type = False
     time = '4'
-    lang = 'C/test_data/' + time
-    
-    path = os.path.join(lang, 'api_slices.txt')
-    list_all_apilabel, list_all_vulline = make_label(path, dict_vuln2testcase, _type)
-    dec_path = os.path.join(lang, 'api_slices_label.pkl')
-    f = open(dec_path, 'wb')
-    pickle.dump(list_all_apilabel, f, True)
-    f.close()
-    dec_path = os.path.join(lang, 'api_slices_vulline.pkl')
-    f = open(dec_path, 'wb')
-    pickle.dump(list_all_vulline, f)
-    f.close()
-    
-    path = os.path.join(lang, 'arraysuse_slices.txt')
-    list_all_arraylabel = make_label(path, dict_vuln2testcase, _type)
-    dec_path = os.path.join(lang, 'array_slice_label.pkl')
-    f = open(dec_path, 'wb')
-    pickle.dump(list_all_arraylabel, f, True)
-    f.close()
-    
-    path = os.path.join(lang, 'pointersuse_slices.txt')
-    list_all_pointerlabel = make_label(path, dict_vuln2testcase, _type)
-    dec_path = os.path.join(lang, 'pointer_slice_label.pkl')
-    f = open(dec_path, 'wb')
-    pickle.dump(list_all_pointerlabel, f, True)
-    f.close()
- 
-    path = os.path.join(lang, 'integeroverflow_slices.txt')
-    list_all_exprlabel = make_label(path, dict_vuln2testcase, _type)
-    dec_path = os.path.join(lang, 'expr_slice_label.pkl')
-    f = open(dec_path, 'wb')
-    pickle.dump(list_all_exprlabel, f, True)
-    f.close()
-    
+    lang = 'test_data/' + time
+
+    # HF: keys are prefixed with zero's which causes them not te be found in make_label()
+    #     this strips the leading zero's of the dictionary keys
+    dict_vuln2testcase = {key.lstrip('0') : value
+                     for key, value in dict_vuln2testcase.iteritems()}
+
+    path = os.path.join(lang, 'slices/api_slices.txt')
+    if os.path.isfile(path):
+        list_all_apilabel, list_all_vulline = make_label(path, dict_vuln2testcase, _type)
+        dec_path = os.path.join(lang, 'labels/api_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_apilabel, f, True)
+        f.close()
+        dec_path = os.path.join(lang, 'vulnlines/api_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_vulline, f)
+        f.close()
+
+    path = os.path.join(lang, 'slices/arraysuse_slices.txt')
+    if os.path.isfile(path):
+        list_all_arraylabel, list_all_vulline = make_label(path, dict_vuln2testcase, _type)
+        dec_path = os.path.join(lang, 'labels/arraysuse_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_arraylabel, f, True)
+        f.close()
+        dec_path = os.path.join(lang, 'vulnlines/arraysuse_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_vulline, f)
+        f.close()
+
+    path = os.path.join(lang, 'slices/pointersuse_slices.txt')
+    if os.path.isfile(path):
+        list_all_pointerlabel, list_all_vulline = make_label(path, dict_vuln2testcase, _type)
+        dec_path = os.path.join(lang, 'labels/pointersuse_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_pointerlabel, f, True)
+        f.close()
+        dec_path = os.path.join(lang, 'vulnlines/pointersuse_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_vulline, f)
+        f.close()
+
+    path = os.path.join(lang, 'slices/integeroverflow_slices.txt')
+    if os.path.isfile(path):
+        list_all_exprlabel, list_all_vulline = make_label(path, dict_vuln2testcase, _type)
+        dec_path = os.path.join(lang, 'labels/integeroverflow_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_exprlabel, f, True)
+        f.close()
+        dec_path = os.path.join(lang, 'vulnlines/integeroverflow_slices.pkl')
+        f = open(dec_path, 'wb')
+        pickle.dump(list_all_vulline, f)
+        f.close()
+
 
 if __name__ == '__main__':
     main()
